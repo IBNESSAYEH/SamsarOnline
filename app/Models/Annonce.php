dc523941af8a49455243f9410f2fb3b46ad2f0dc
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2024-02-17 13:32:30 +0100

    add models/factories/controllers

---
+++ app/Models/Annonce.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Annonce extends Model
+{
+    use HasFactory;
+
+
+    
+}
